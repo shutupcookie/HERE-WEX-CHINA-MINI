105e329887db008115570d1c5f36c4442907d69b
Shutup.Cookie <146734606+shutupcookie@users.noreply.github.com>
2025-09-10 21:52:35 +0200

    Add Header component for site navigation

---
+++ app/components/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <header className="site-header">
+      <div className="site-header__inner">
+        <h1 className="site-header__title">HERE WEX China Mini</h1>
+        <nav className="site-header__nav">
+          <a href="/">Home</a>
+          <a href="/blog">Blog</a>
+          <a href="/digital-cockpit">Digital Cockpit</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
